99d403fbac475269051f9b4093e0f92e4ea5c04c
Benioqu <93092692+Benioqu@users.noreply.github.com>
2025-05-02 10:09:44 +0200

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -37,3 +37,19 @@ complex multiply(complex z1, complex z2)
 	z3.im = z1.re * z2.im + z1.im * z2.re;
 	return z3;
 }
+
+complex divide(complex z1, complex z2)
+{
+	complex z3;
+	double denominator = z2.re * z2.re + z2.im * z2.im;
+	if(denominator == 0)
+	{
+		cout << "Blad: dzielenie przez zero!" << endl;
+		z3.re = 0;
+		z3.im = 0;
+		return z3;
+	}
+	z3.re = (z1.re * z2.re + z1.im * z2.im);
+	z3.im = (z1.im * z2.re - z1.re * z2.im);
+	return z3;
+}
