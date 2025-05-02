900b3ded762c36b5cdad8bdcf38c927446222c87
Benioqu <93092692+Benioqu@users.noreply.github.com>
2025-05-02 10:04:43 +0200

    Update complex.h

--- complex.h
+++ complex.h
@@ -11,3 +11,4 @@ void print(complex z);
 complex add(complex z1, complex z2);
 complex substract(complex z1, complex z2);
 complex multiply(complex z1, complex z2);
+complex divide(complex z1, complex z2);
